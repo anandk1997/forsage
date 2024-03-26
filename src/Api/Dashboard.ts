fdf3ee01806416a8c2cee9fbcf21ed86db54f618
anand <anandk01997@gmail.com>
2024-03-26 23:10:29 +0530

    userinfo fix

--- src/Api/Dashboard.ts
+++ src/Api/Dashboard.ts
@@ -1,8 +1,12 @@
 import { authToken, axiosInstance } from "src/Lib/utils";
 
-export const getDashboard = async () => {
+export const getDashboard = async (
+  setUserInfo: (newUserInfo: object) => void
+) => {
   try {
     const response = await axiosInstance.get("api/v1/dashboard", authToken());
+
+    setUserInfo(response?.data?.userInfo[0]);
 
     return response?.data;
   } catch (error) {
