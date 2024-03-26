787a74c6b4be184e1f48ce5ab5b7e147588eb83a
Inder <indersein416@gmail.com>
2024-03-26 17:52:06 +0530

    update code

--- src/Components/Loading.tsx
+++ src/Components/Loading.tsx
@@ -3,7 +3,7 @@ import { CircularProgress } from "@mui/material";
 export const Loading = () => {
   return (
     <div className="flex justify-center items-center h-[100vh] bg-gray">
-      <CircularProgress sx={{ scale: ".8", color: "white !important" }} />;
+      <CircularProgress sx={{ scale: ".8", color: "white !important" }} />
     </div>
   );
 };
